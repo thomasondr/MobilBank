6cd3bce12db4977839eb18ef929f1635e24b1b39
Ondresik, Tamas <thomas.ondr@gmail.com>
2018-01-18 00:48:06 +0100

    Initial Commit

---
+++ MobileBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileBank
+//
+//  Created by Tamas Ondresik on 2018. 01. 18..
+//  Copyright © 2018. Tamas Ondresik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
