c2004df0a0b476881117d31ad2e8ac5d6503e663
Ondresik, Tamas <thomas.ondr@gmail.com>
2018-01-19 01:14:44 +0100

    progress

---
+++ MobileBank/MenuTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MenuTableViewCell.swift
+//  MobileBank
+//
+//  Created by Tamas Ondresik
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var menuCellView: UIView!
+    @IBOutlet weak var menuCellImage: UIImageView!
+    @IBOutlet weak var menuCellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MobileBank/MenuViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MenuViewController.swift
+//  MobileBank
+//
+//  Created by Tamas Ondresik
+//
+
+import UIKit
+
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let menuItems = ["Accounts", "Transactions", "Payment","User","Contact"]
+    let rowHeight:CGFloat = 50
+    let menuCellId = "menuCell"
+    
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        menuTableView.tableFooterView = UIView(frame: CGRect.zero)
+        menuTableView.separatorColor = UIColor.clear
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        menuTableView.sizeToFit()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return rowHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = menuTableView.dequeueReusableCell(withIdentifier: menuCellId) as! MenuTableViewCell
+        
+        let menuItem:String = menuItems[indexPath.row]
+        
+        cell.menuCellLabel.text = menuItem
+        cell.menuCellImage.image = UIImage(named: menuItem)
+        
+        cell.menuCellView.layer.cornerRadius = cell.menuCellView.frame.height/4
+        
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowIndex = indexPath.row
+        let segueId = menuItems[rowIndex]
+        
+        performSegue(withIdentifier: segueId, sender: self)
+        
+    }
+
+
+}
