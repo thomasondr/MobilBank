e229e6aad7f96276c0b6ef2d6c884c7843eb1e88
Ondresik, Tamas <thomas.ondr@gmail.com>
2018-01-19 02:34:51 +0100

    atm search view

---
+++ MobileBank/ATMSearchViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ATMSearchViewController.swift
+//  MobileBank
+//
+//  Created by Tamas Ondresik
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ATMSearchViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let manager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.05,0.05)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+    }
+    
+    
+
+}
